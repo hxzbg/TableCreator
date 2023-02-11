8316aaa309994d30d6f469cf51687f570282825d
hxz <361784154@qq.com>
2023-02-11 10:59:24 +0800

    修正语法检查逻辑

--- TableCreator/FormatOperatorChecker.cs
+++ TableCreator/FormatOperatorChecker.cs
@@ -7,7 +7,7 @@ public class FormatOperatorChecker
 {
 	int _offset = 1;
 	ExcelParser _parser = null;
-	List<string>[] _formats = null;
+	Dictionary<string, bool>[] _formats = null;
 	public FormatOperatorChecker(ExcelParser parser, int offset = 1)
 	{
 		_parser = parser;
@@ -71,6 +71,7 @@ public class FormatOperatorChecker
 		{
 			for (int index = 0; index < line.Length; index++)
 			{
+                /*
 				if (line[index] == '%')
 				{
 					index++;
@@ -83,20 +84,21 @@ public class FormatOperatorChecker
 						}
 					}
 				}
-				else if (line[index] == '{' && (index + 2) < line.Length && line[index + 2] == '}')
+				else */if (line[index] == '{' && (index + 2) < line.Length && line[index + 2] == '}')
 				{
 					parser(index, 1);
-					index += 3;
+					index += 2;
 				}
 			}
 		}
 	}
 
-	void ExtraFormaters(string line, List<string> formaters)
+	void ExtraFormaters(string line, Dictionary<string, bool> formaters)
 	{
 		formaters.Clear();
 		for(int index = 0; index < line.Length; index ++)
 		{
+            /*
 			if(line[index] == '%')
 			{
 				index++;
@@ -105,32 +107,33 @@ public class FormatOperatorChecker
 					int result = _FormatCheckers[i](line, index);
 					if(result > 0)
 					{
-						formaters.Add(line.Substring(index - 1, result + 1));
+						formaters[line.Substring(index - 1, result + 1)] = true;
 					}
 				}
 			}
-			else if(line[index] == '{' && (index + 2) < line.Length && line[index + 2] == '}')
+			else */if(line[index] == '{' && (index + 2) < line.Length && line[index + 2] == '}')
 			{
-				formaters.Add(line.Substring(index, 3));
-				index += 3;
+				formaters[line.Substring(index, 3)] = true;
+				index += 2;
 			}
 		}
 	}
 
 	List<int> _unMatchFileds = new List<int>();
-	void AppendFormats(StringBuilder stringBuilder, List<string>[] formats, int row, int field)
+	void AppendFormats(StringBuilder stringBuilder, Dictionary<string, bool>[] formats, int row, int field)
 	{
-		List<string> list = formats[field];
+		Dictionary<string, bool> list = formats[field];
 		stringBuilder.AppendFormat("\t\t{0}:", _parser.GetFieldName(field));
 		stringBuilder.AppendLine(_parser.GetString(row, field));
 		stringBuilder.Append("\t\tFormat:");
 		if(list.Count > 0)
 		{
-			for (int index = 0; index < list.Count; index++)
-			{
-				stringBuilder.Append(list[index]);
-				stringBuilder.Append(",");
-			}
+            Dictionary<string, bool>.Enumerator iter = list.GetEnumerator();
+            while(iter.MoveNext())
+            {
+                stringBuilder.Append(iter.Current.Key);
+                stringBuilder.Append(",");
+            }
 			stringBuilder.Remove(stringBuilder.Length - 1, 1);
 		}
 		stringBuilder.AppendLine();
@@ -151,7 +154,7 @@ public class FormatOperatorChecker
 			ExtraFormaters(line, _formats[index]);
 		}
 
-		List<string> src = _formats[_offset];
+		Dictionary<string, bool> src = _formats[_offset];
 		for (int index = _offset + 1; index < _formats.Length; index++)
 		{
 			string line = _parser.GetString(row, index);
@@ -161,21 +164,22 @@ public class FormatOperatorChecker
 			}
 
 			bool unMatch = false;
-			List<string> list = _formats[index];
+			Dictionary<string, bool> list = _formats[index];
 			if (list.Count != src.Count)
 			{
 				unMatch = true;
 			}
 			else
 			{
-				for (int i = 0; i < src.Count; i++)
-				{
-					if (list[i] != src[i])
-					{
-						unMatch = true;
-						break;
-					}
-				}
+                Dictionary<string, bool>.Enumerator iter = src.GetEnumerator();
+                while(iter.MoveNext())
+                {
+                    if(!list.ContainsKey(iter.Current.Key))
+                    {
+                        unMatch = true;
+                        break;
+                    }
+                }
 			}
 
 			if (unMatch)
@@ -204,10 +208,10 @@ public class FormatOperatorChecker
 			return "";
 		}
 
-		_formats = new List<string>[_parser.FieldCount];
+		_formats = new Dictionary<string, bool>[_parser.FieldCount];
 		for(int index = 0; index < _formats.Length; index ++)
 		{
-			_formats[index] = new List<string>();
+			_formats[index] = new Dictionary<string, bool>();
 		}
 
 		bool hasError = false;
