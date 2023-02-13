b04e19fb22fc284d492c75e171d44fd2c0e80f25
hxz <361784154@qq.com>
2023-02-13 20:07:24 +0800

    加强语法检查逻辑

--- TableCreator/FormatOperatorChecker.cs
+++ TableCreator/FormatOperatorChecker.cs
@@ -7,7 +7,7 @@ public class FormatOperatorChecker
 {
 	int _offset = 1;
 	ExcelParser _parser = null;
-	Dictionary<string, bool>[] _formats = null;
+	Dictionary<string, int>[] _formats = null;
 	public FormatOperatorChecker(ExcelParser parser, int offset = 1)
 	{
 		_parser = parser;
@@ -65,70 +65,109 @@ public class FormatOperatorChecker
 
 	static System.Func<string, int, int>[] _FormatCheckers = { ThreeCharChecker, TwoCharChecker, OneCharChecker };
 
-	public static void ParseFormaters(string line, System.Action<int, int> parser)
-	{
-		if(string.IsNullOrEmpty(line) == false && parser != null)
-		{
-			for (int index = 0; index < line.Length; index++)
-			{
-                /*
-				if (line[index] == '%')
-				{
-					index++;
-					for (int i = 0; i < _FormatCheckers.Length; i++)
-					{
-						int result = _FormatCheckers[i](line, index);
-						if (result > 0)
-						{
-							parser(index - 1, result);
-						}
-					}
-				}
-				else */if (line[index] == '{' && (index + 2) < line.Length && line[index + 2] == '}')
-				{
-					parser(index, 1);
-					index += 2;
-				}
-			}
-		}
-	}
+    static void FindFormat(string line, System.Action<int, int, int ,string> handler)
+    {
+        if(string.IsNullOrEmpty(line))
+        {
+            return;
+        }
 
-	void ExtraFormaters(string line, Dictionary<string, bool> formaters)
+        int startindex = 0;
+        while(startindex >= 0)
+        {
+            int start = line.IndexOf('{', startindex);
+            startindex = start;
+            if (start >= 0)
+            {
+                startindex++;
+                int end_pos = line.IndexOf('}', start);
+                if (end_pos > start)
+                {
+                    string test = line.Substring(start, end_pos - start + 1);
+                    int symbol = test.IndexOf(':');
+                    if (symbol < 0)
+                    {
+                        //没有找到 ':'
+                        int status = 0;
+                        for(int i = start + 1; i < end_pos; i ++)
+                        {
+                            if(line[i] >= '0' && line[i] <= '9')
+                            {
+                                status |= 1;
+                            }
+                            else if((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z'))
+                            {
+                                status |= 2;
+                            }
+                        }
+
+                        if(status == 1)
+                        {
+                            int id = 0;
+                            string number = test.Substring(1, test.Length - 2);
+                            if(int.TryParse(number, out id))
+                            {
+                                handler(start, end_pos, id, test);
+                            }
+                        }
+                        else if(status == 2)
+                        {
+                            handler(start, end_pos, -1, test);
+                        }
+                    }
+                    else if (symbol > 0)
+                    {
+                        int status = 0;
+                        for (int i = 1; i < symbol; i++)
+                        {
+                            if (test[i] >= '0' && test[i] <= '9')
+                            {
+                                status |= 1;
+                            }
+                            else if ((test[i] >= 'a' && test[i] <= 'z') || (test[i] >= 'A' && test[i] <= 'Z'))
+                            {
+                                status |= 2;
+                            }
+                        }
+
+                        if (status == 1)
+                        {
+                            int id = 0;
+                            string number = test.Substring(1, symbol - 1);
+                            if (int.TryParse(number, out id))
+                            {
+                                handler(start, end_pos, id, test);
+                            }
+                        }
+                        else if (status == 2)
+                        {
+                            handler(start, end_pos, -1, test);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+	void ExtraFormaters(string line, Dictionary<string, int> formaters)
 	{
 		formaters.Clear();
-		for(int index = 0; index < line.Length; index ++)
-		{
-            /*
-			if(line[index] == '%')
-			{
-				index++;
-				for(int i = 0; i < _FormatCheckers.Length; i ++)
-				{
-					int result = _FormatCheckers[i](line, index);
-					if(result > 0)
-					{
-						formaters[line.Substring(index - 1, result + 1)] = true;
-					}
-				}
-			}
-			else */if(line[index] == '{' && (index + 2) < line.Length && line[index + 2] == '}')
-			{
-				formaters[line.Substring(index, 3)] = true;
-				index += 2;
-			}
-		}
+        FindFormat(line, delegate (int start, int end, int id, string format)
+        {
+            formaters[format] = id;
+        });
 	}
 
 	List<int> _unMatchFileds = new List<int>();
-	void AppendFormats(StringBuilder stringBuilder, Dictionary<string, bool>[] formats, int row, int field)
+	void AppendFormats(StringBuilder stringBuilder, Dictionary<string, int>[] formats, int row, int field)
 	{
-		Dictionary<string, bool> list = formats[field];
+		Dictionary<string, int> list = formats[field];
 		stringBuilder.AppendFormat("\t\t{0}:", _parser.GetFieldName(field));
 		stringBuilder.AppendLine(_parser.GetString(row, field));
 		stringBuilder.Append("\t\tFormat:");
 		if(list.Count > 0)
 		{
-            Dictionary<string, bool>.Enumerator iter = list.GetEnumerator();
+            Dictionary<string, int>.Enumerator iter = list.GetEnumerator();
             while(iter.MoveNext())
             {
                 stringBuilder.Append(iter.Current.Key);
@@ -139,6 +178,7 @@ public class FormatOperatorChecker
 		stringBuilder.AppendLine();
 	}
 
+    Dictionary<int, bool> _formatIndexs = new Dictionary<int, bool>();
 	bool CheckRow(int row, StringBuilder stringBuilder)
 	{
 		_unMatchFileds.Clear();
@@ -154,7 +194,7 @@ public class FormatOperatorChecker
 			ExtraFormaters(line, _formats[index]);
 		}
 
-		Dictionary<string, bool> src = _formats[_offset];
+		Dictionary<string, int> src = _formats[_offset];
 		for (int index = _offset + 1; index < _formats.Length; index++)
 		{
 			string line = _parser.GetString(row, index);
@@ -164,14 +204,14 @@ public class FormatOperatorChecker
 			}
 
 			bool unMatch = false;
-			Dictionary<string, bool> list = _formats[index];
+			Dictionary<string, int> list = _formats[index];
 			if (list.Count != src.Count)
 			{
 				unMatch = true;
 			}
 			else
 			{
-                Dictionary<string, bool>.Enumerator iter = src.GetEnumerator();
+                Dictionary<string, int>.Enumerator iter = src.GetEnumerator();
                 while(iter.MoveNext())
                 {
                     if(!list.ContainsKey(iter.Current.Key))
@@ -188,7 +228,7 @@ public class FormatOperatorChecker
 			}
 		}
 
-		if(_unMatchFileds.Count > 0)
+        if (_unMatchFileds.Count > 0)
 		{
 			stringBuilder.AppendFormat("{0}行出错:\n", row + 1);
 			AppendFormats(stringBuilder, _formats, row, _offset);
@@ -198,7 +238,33 @@ public class FormatOperatorChecker
 			}
 			return false;
 		}
-		return true;
+        else if (src.Count > 0)
+        {
+            //检查format顺序是否正确
+            int maxindex = -1;
+            _formatIndexs.Clear();
+            Dictionary<string, int>.Enumerator iter = src.GetEnumerator();
+            while (iter.MoveNext())
+            {
+                int v = iter.Current.Value;
+                if(v >= 0)
+                {
+                    _formatIndexs[v] = true;
+                }
+
+                if (v > maxindex)
+                {
+                    maxindex = v;
+                }
+            }
+
+            if(maxindex + 1 != _formatIndexs.Count)
+            {
+                stringBuilder.AppendFormat("{0}行出错:\n", row + 1);
+                AppendFormats(stringBuilder, _formats, row, _offset);
+            }
+        }
+        return true;
 	}
 
 	public string Run()
@@ -208,10 +274,10 @@ public class FormatOperatorChecker
 			return "";
 		}
 
-		_formats = new Dictionary<string, bool>[_parser.FieldCount];
+		_formats = new Dictionary<string, int>[_parser.FieldCount];
 		for(int index = 0; index < _formats.Length; index ++)
 		{
-			_formats[index] = new Dictionary<string, bool>();
+			_formats[index] = new Dictionary<string, int>();
 		}
 
 		bool hasError = false;
